1a5ec4f1e09ef2cddc0a29f6847959d4679edfb5
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-18 12:42:36 +0700

    Update __init__.py

--- rams/__init__.py
+++ rams/__init__.py
@@ -223,7 +223,7 @@ BOTLOG_MSG = os.environ.get("BOTLOG_MSG") or f"```💢 RAM - UBOT DAH AKTIF KONT
 # Devg For gesss
 DEVG = [
     910766621, #rama
-    5155140917, #vicky
+    874946835, #vicky
     1977874449, #lunar
     844432220, #risman
     2003295492,
