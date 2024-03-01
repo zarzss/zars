b69e350f76f263a775027ced03ef578a708aef2d
zarzss <161209271+zarzss@users.noreply.github.com>
2024-03-01 20:05:48 +0700

    Update ping.py

--- rams/modules/ping.py
+++ rams/modules/ping.py
@@ -154,7 +154,7 @@ async def redis(pingx):
     await pingx.reply(
             f"**⛥ ᴘɪɴɢ** `%sms`\n"
             f"**⛥ ᴜᴘᴛɪᴍᴇ** `{uptime}`\n"
-            f"**⛥ ᴍᴇɴᴛɪᴏɴ [{user.first_name}](tg://user?id={user.id}**" % (duration)
+            f"**⛥ ᴍᴇɴᴛɪᴏɴ [{user.first_name}](tg://user?id={user.id})**" % (duration)
     )
 
 def speed_convert(size):
